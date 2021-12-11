20208c4bb9f650f89c6d426e2de05861be4b20d0
Joao Gomes <joaopfg.2002@gmail.com>
2021-12-11 18:42:48 +0000

    Refactoring with pipe operator and pattern matching

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -1,5 +1,11 @@
 defmodule ReportsGenerator do
   def build(filename) do
-    file =  File.read("reports/#{filename}")
+    "reports/#{filename}"
+    |> File.read()\
+    |> handle_file()
   end
+
+  defp handle_file({:ok , file_content}), do: file_content
+  defp handle_file({:error , _}), do: "Error while opening file!"
+
 end
